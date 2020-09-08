4abd044078bfbe840d25a4a383c3991a5291ae03
harrychanhoyin95 <harrychanhoyin95@gmail.com>
2020-09-09 01:41:50 +0800

    feat: row scroll-in animation

--- client/src/components/AppListing/AppListing.js
+++ client/src/components/AppListing/AppListing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Rating from 'react-rating'
 import LazyLoad from 'react-lazyload';
 import InfiniteScroll from "react-infinite-scroll-component";
+import Fade from 'react-reveal/Fade';
 import { MoonLoader } from "react-spinners";
 
 import './AppListing.scss';
@@ -35,30 +36,32 @@ const AppListing = ({
         {freeApps.map((apps, index) => {
           return (
             <LazyLoad key={apps.title} height={100}>
-              <div className="app-listing_apps-container">
-                <div className="app-listing_apps-container-border-bottom">
-                  <div className="app-listing_index">{index + 1}</div>
-                  <img
-                    src={apps.images.artworkUrl100} 
-                    alt={apps.title}
-                    className={`app-listing_image ${index % 2 === 0 ? "odd-image" : "even-image" }`}
-                  />
-                  <div className="app-listing_content">
-                    <div className="app-listing_title">{apps.title}</div>
-                    <div className="app-listing_category">{apps.category}</div>
-                    <div className="app-listing_rating">
-                      <Rating
-                        readonly
-                        quiet
-                        initialRating={apps.rating}
-                        emptySymbol={<i className="far fa-star empty-star"></i>}
-                        fullSymbol={<i className="fas fa-star full-star"></i>}
-                      />
-                      <span className="app-listing_rating-count">({apps.ratingCount})</span>
+              <Fade left>
+                <div className="app-listing_apps-container">
+                  <div className="app-listing_apps-container-border-bottom">
+                    <div className="app-listing_index">{index + 1}</div>
+                    <img
+                      src={apps.images.artworkUrl100} 
+                      alt={apps.title}
+                      className={`app-listing_image ${index % 2 === 0 ? "odd-image" : "even-image" }`}
+                    />
+                    <div className="app-listing_content">
+                      <div className="app-listing_title">{apps.title}</div>
+                      <div className="app-listing_category">{apps.category}</div>
+                      <div className="app-listing_rating">
+                        <Rating
+                          readonly
+                          quiet
+                          initialRating={apps.rating}
+                          emptySymbol={<i className="far fa-star empty-star"></i>}
+                          fullSymbol={<i className="fas fa-star full-star"></i>}
+                        />
+                        <span className="app-listing_rating-count">({apps.ratingCount})</span>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              </Fade>
             </LazyLoad>
           )
         })}
